a3cc089ff02b53d520c29c51a488fe930153c18c
Tim Loden <tim.m.loden@gmail.com>
2020-08-20 18:11:08 -0700

    completed home page updates and brands addition

--- page-home.php
+++ page-home.php
@@ -59,31 +59,13 @@ get_header();
 <div id="choose-your-vehicle" class="bg-light border-bottom">
     <div class="container py-3">
         <div class="row align-items-center">
-            <div class="col-12 col-lg-2">
-                <p class="mb-0 text-primary font-weight-bolder">Choose your vehicle:</p>
-            </div>
-            <div class="col-12 col-lg-10">
+            <div class="col-12">
                 <div class="home-ymm">
-                    <?php echo facetwp_display('facet', 'year_make_model'); ?>
+                    <?php //echo facetwp_display('facet', 'year_make_model'); ?>
+                    <p class="mb-0 text-center"><a class="btn-lg btn btn-primary"
+                            href="<?php echo site_url(); ?>/shop#ymm-bar">Choose your Vehicle</a></p>
                 </div>
             </div>
-        </div>
-        <div class="d-none">
-            <?php
-            $args = array(
-                'post_type' => 'product',
-                'posts_per_page' => 12,
-            
-                );
-            $loop = new WP_Query( $args );
-            
-            if ( $loop->have_posts() ) {
-                while ( $loop->have_posts() ) : $loop->the_post();
-                    wc_get_template_part( 'content', 'product' );
-                endwhile;
-            }
-            wp_reset_postdata();
-            ?>
         </div>
     </div>
 </div>
@@ -92,11 +74,9 @@ get_header();
     <div class="container py-5">
         <div class="row home-benefits">
             <div class="col-12 pb-5">
-                <h1 class="text-center h2 pb-3">Classic car and restoration auto parts location at
-                    everyday low
-                    prices.</h1>
-                <p class="text-center pb-4">Classic Body Parts is the best choice for Auto Restoration and Performance
-                    car
+                <h1 class="text-center h2 pb-3">Restoration Performance - classic car and restoration auto parts at low
+                    prices everyday!</h1>
+                <p class="text-center pb-4">Your choice for auto restoration and performance car
                     parts because of our
                     respective associations as a dealer from over 50 aftermarket and restoration parts manufacturers to
                     meet all your vehicle's needs. </p>
@@ -104,7 +84,7 @@ get_header();
             <div class="col-12 col-lg-4 text-center">
                 <i class="las la-shipping-fast text-primary"></i>
                 <h5 class="my-2 font-weight-bold">$7.50 Shipping</h5>
-                <p>Ground orders over $150 ship for only $7.50! Freight shipping starts at $135!</p>
+                <p>Ground orders over $150 ship for only $7.50! <strong>Freight only $130!</strong></p>
             </div>
             <div class="col-12 col-lg-4 text-center">
                 <i class="las la-check-circle text-primary"></i>
@@ -124,36 +104,63 @@ get_header();
         </div>
     </div>
 </div>
+<div class="container">
+    <div class="row align-items-center py-5">
+        <div class=" col-12 col-lg-5 order-2 order-lg-1">
+            <img class="img-fluid"
+                src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/home-page-chevelle.jpg">
 
-<div class="row align-items-center no-gutters py-5">
-    <div class=" col-12 col-lg-6 order-2 order-lg-1">
-        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/shelby-cobra.jpg">
-
-    </div>
-    <div class="col-12 col-lg-6 order-1 order-lg-2 mb-5 mb-lg-5">
-        <div class="px-3">
-            <h2 class="pb-3 mb-3 title-border">Why we should be your first choice for restoration &amp; muscle car parts
-            </h2>
-            <p>We have a history as one of the largest dealers for Dynacorn International. Dynacorn has been supplying
-                the classic muscle car enthusiast quality sheet metal, bright trim and molding since 1984.
-            </p>
-            <p>
-                We strive to meet all your classic car and truck vehicles needs. Don&#39;t be fooled by other companies
-                that
-                offer monthly or weekend sells offering discounts at 15 - 25% off RETAIL price! We have the lowest
-                prices every month all year long! Just try us and you will see.</p>
-
-            <a href="<?php echo site_url(); ?>/shop#ymm-bar" class="btn btn-primary">Browse parts for your vehicle</a>
         </div>
+        <div class="col-12 col-lg-7 order-1 order-lg-2 mb-5 mb-lg-5">
+            <div class="px-3">
+                <h2 class="pb-3 mb-3 title-border">Why Restoration Performance should be your first choice for classic
+                    &amp;
+                    muscle car parts
+                </h2>
+                <p>
+                    We strive to meet all your classic car and truck vehicles needs. Don&#39;t be fooled by other
+                    companies
+                    that
+                    offer monthly or weekend sells offering discounts at 15 - 25% off RETAIL price! We have the lowest
+                    prices every month all year long! Just try us and you will see.</p>
 
+                <a href="<?php echo site_url(); ?>/shop#ymm-bar" class="btn btn-primary">Browse parts for your
+                    vehicle</a>
+            </div>
+
+        </div>
     </div>
 </div>
+<?php if( have_rows('home_brands') ): ?>
+<div class="border-bottom border-top py-5">
+    <div class="container">
+        <h3 class="text-center pt-3">Some of our featured brands</h3>
+        <p class="text-center pb-3">The Classic Body Parts resource center is here to help answer all of your
+            restoration questions.</p>
+        <div class="row align-items-center justify-content-center">
+            <?php while( have_rows('home_brands') ): the_row(); 
+
+            $image = get_sub_field('brand_image');
+
+        ?>
+            <div class="col-6 col-md-3 text-center pb-3">
+                <img class="img-fluid" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+            </div>
+            <?php endwhile; ?>
+        </div>
+        <div class="text-center pt-5 pt-lg-3">
+            <a href="<?php echo site_url(); ?>/shop#ymm-bar" class="btn btn-primary">Select your vehicle to get
+                started</a>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
 
 <div class="border-bottom pb-5">
     <div class="container py-5">
         <h3 class="text-center pt-3">Need help with your project?</h3>
-        <p class="text-center pb-5">The Classic Body Parts resource center is here to help answer all of your
-            restoration questions.</p>
+        <p class="text-center pb-5">The Restoration Performance resource center is here to help answer all of your
+            classic car questions.</p>
         <div class="row">
             <div class="col-12 col-lg-6 mb-5 mb-lg-0">
                 <div class="card home-resource py-5"
@@ -173,7 +180,8 @@ get_header();
                     style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/images/firebird.jpg);">
                     <div class="card-body position-relative">
                         <h4 class="text-white text-center h2">Frequently Asked Questions</h4>
-                        <p class="text-white text-center">Common questions we get about ordering</p>
+                        <p class="text-white text-center">Common questions we get about ordering from Restoration
+                            Performance</p>
                         <a class="text-white text-center d-block mt-5"
                             href="<?php echo site_url(); ?>/frequently-asked-questions/">Browse Frequently
                             Asked Questions</a>
