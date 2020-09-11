dee04a035a3d94f848ee9c71c0d0aaafc60a753e
Tim Loden <tim.m.loden@gmail.com>
2020-09-11 08:55:44 -0700

    added dakota digital discount notice to product page

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -17,3 +17,20 @@ function change_woocommerce_order_number( $order_id ) {
     $new_order_id = $prefix . $order_id;
     return $new_order_id;
 }
+
+// dakota digital 5% message
+
+add_action( 'woocommerce_before_add_to_cart_form', 'action_woocommerce_before_add_to_cart_form', 10, 0 ); 
+
+// define the woocommerce_before_add_to_cart_form callback 
+function action_woocommerce_before_add_to_cart_form() { 
+    global $product;
+    $id = $product->get_id();
+    $brand = get_brand_name($id);
+    
+    if ($brand == 'Dakota Digital') {
+        echo '<p class="font-weight-bold" style="color: #ef9020;">SAVE 5% MORE WHEN ADDED TO CART</p>';
+    }
+    
+}; 
+        
