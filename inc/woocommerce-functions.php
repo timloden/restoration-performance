645db32f0e181f752285d89d2ff4140fabcd9044
Tim Loden <tim.m.loden@gmail.com>
2020-09-11 09:03:56 -0700

    added goodmark product cbp message

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -20,10 +20,10 @@ function change_woocommerce_order_number( $order_id ) {
 
 // dakota digital 5% message
 
-add_action( 'woocommerce_before_add_to_cart_form', 'action_woocommerce_before_add_to_cart_form', 10, 0 ); 
+add_action( 'woocommerce_before_add_to_cart_form', 'dakota_digital_discount_message', 10, 0 ); 
 
 // define the woocommerce_before_add_to_cart_form callback 
-function action_woocommerce_before_add_to_cart_form() { 
+function dakota_digital_discount_message() { 
     global $product;
     $id = $product->get_id();
     $brand = get_brand_name($id);
@@ -33,4 +33,21 @@ function action_woocommerce_before_add_to_cart_form() {
     }
     
 }; 
+
+// Goodmark CBP message
+
+add_action( 'woocommerce_after_single_product', 'goodmark_cbp_product_message', 10, 0 ); 
+
+// define the woocommerce_before_add_to_cart_form callback 
+function goodmark_cbp_product_message() { 
+    global $product;
+    $id = $product->get_id();
+    $brand = get_brand_name($id);
+    
+    if ($brand == 'Goodmark') {
+        echo '<div class="alert alert-primary text-center">Looking for more body parts? <a class="font-weight-bold" style="color: #000;" href="https://classicbodyparts.com/">Checkout Classic Body Parts for more classic car body parts</a>!</div>';
+    }
+    
+}; 
+    
         
