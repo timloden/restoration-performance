c077f0cb204a39361f5f9ba0b6e900cff592ba96
Tim Loden <tim.m.loden@gmail.com>
2021-08-09 07:04:02 -0700

    updated cart to match CBP

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -60,19 +60,16 @@ if (in_array('oer-freight', $shipping_classes) && in_array('goodmark-freight', $
 <tr class="woocommerce-shipping-totals shipping">
     <td style="border-top: none;" colspan="2" data-title="<?php echo esc_attr($package_name); ?>">
         <?php if ($available_methods) : ?>
-        <ul id="shipping_method" class="woocommerce-shipping-methods list-unstyled mb-2">
+        <ul id="shipping_method" class="woocommerce-shipping-methods list-unstyled my-2">
             <?php foreach ($available_methods as $method) : ?>
             <li class="mb-1">
                 <div class="form-check">
                     <?php
-							if (1 < count($available_methods)) {
-								printf('<input type="radio" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method form-check-input" %4$s />', $index, esc_attr(sanitize_title($method->id)), esc_attr($method->id), checked($method->id, $chosen_method, false)); // WPCS: XSS ok.
-							} else {
-								printf('<input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" />', $index, esc_attr(sanitize_title($method->id)), esc_attr($method->id)); // WPCS: XSS ok.
-							}
-							printf('<label class="form-check-label d-flex justify-content-between" for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr(sanitize_title($method->id)), wc_cart_totals_shipping_method_label($method)); // WPCS: XSS ok.
-							do_action('woocommerce_after_shipping_rate', $method, $index);
-							?>
+                        printf('<input type="radio" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method form-check-input" %4$s />', $index, esc_attr(sanitize_title($method->id)), esc_attr($method->id), checked($method->id, $chosen_method, false)); // WPCS: XSS ok.
+                        printf('<label class="form-check-label d-flex justify-content-between" for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr(sanitize_title($method->id)), wc_cart_totals_shipping_method_label($method)); // WPCS: XSS ok.
+                        do_action('woocommerce_after_shipping_rate', $method, $index);
+                                                    
+					?>
 
                 </div>
             </li>
@@ -80,11 +77,11 @@ if (in_array('oer-freight', $shipping_classes) && in_array('goodmark-freight', $
         </ul>
 
         <?php if (is_cart()) : ?>
-        <p class="woocommerce-shipping-destination">
+        <p class="woocommerce-shipping-destination mb-0">
             <?php
 					if ($formatted_destination) {
 						// Translators: $s shipping destination.
-						printf(esc_html__('Shipping to %s.', 'woocommerce') . ' ', '<strong>' . esc_html($formatted_destination) . '</strong>');
+						printf(esc_html__('Shipping to %s.', 'woocommerce') . ' ', '<strong><br>' . esc_html($formatted_destination) . '</strong>');
 						$calculator_text = esc_html__('Change address', 'woocommerce');
 					} else {
 						echo wp_kses_post(apply_filters('woocommerce_shipping_estimate_html', __('Shipping options will be updated during checkout.', 'woocommerce')));
